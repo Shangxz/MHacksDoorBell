3a54641921b726d9ef697c9a35376047f1a272be
Michael Sweetland <mjsweet@umich.edu>
2017-09-24 02:16:08 -0400

    alexa

--- apiAWS/testScripts/uploadPicture.py
+++ apiAWS/testScripts/uploadPicture.py
@@ -2,9 +2,9 @@
 # ===================
 import base64
 import json
-import io
 
 # 3rd Party Dependencies
+# ======================
 import requests
 
 # Globals
@@ -31,8 +31,22 @@ def trainModel(fileName,person):
 		return requests.post(url, data=json.dumps(payload), headers=headers).text
 
 
+def shangAPI(fileName):
+	url = 'http://13.92.254.54:8080/face'
+	with open(fileName, "rb") as f:
+		#e = str(int(f.read(), 8))
+		e = base64.urlsafe_b64encode(f.read())
+		#print e
+		#payload = {"":""}
+		headers = {'Content-type': 'application/json'}
+		return requests.post(url, data=e, headers=headers).text
+
+
+
+
 if __name__ == '__main__':
-	print uploadImage('photoG.jpg')
+	print shangAPI('shang.jpg')
+	#print uploadImage('photoG.jpg')
 	#print trainModel('photoG.jpg','Griffin Miller')
 
 
